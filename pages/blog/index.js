d55073ecc022e3dbb0c8875a4640e4717b0958c6
kurasita you <n23012@std.it-college.ac.jp>
2024-01-10 05:04:44 +0000

    blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,11 @@
+import Meta from 'components/meta'
 import Hero from 'components/hero'
 import Container from 'components/container'
 const Blog = () => {
   const props3 = { title: 'Blog', subtitle: 'Recent Posts' }
   return (
     <Container>
+      <Meta pageTitle='ブログ' pageDesc='ブログの記事' />
       <Hero {...props3} />
     </Container>
   )
