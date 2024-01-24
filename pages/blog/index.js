66247e26381dfd549e9f152b47d1331f28474a43
kurasita you <n23012@std.it-college.ac.jp>
2024-01-24 01:31:36 +0000

    ch9 fin
    それぞれのページに行けるようにした

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,14 +1,38 @@
 import Meta from 'components/meta'
 import Hero from 'components/hero'
 import Container from 'components/container'
-const Blog = () => {
+import { getAllPosts } from 'lib/api'
+import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
+
+const Blog = ({ posts }) => {
   const props3 = { title: 'Blog', subtitle: 'Recent Posts' }
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事' />
       <Hero {...props3} />
+      <Posts posts={posts} />
     </Container>
   )
 }
 
 export default Blog
+
+export async function getStaticProps () {
+  const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,40 @@
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
-const Home = () => {
+import { getAllPosts } from 'lib/api'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
+
+const Home = ({ posts }) => {
   const props1 = { title: 'CUBE', subtitle: 'アウトプットしていくサイト' }
   return (
     <Container>
       <Meta />
       <Hero {...props1} imageOn />
+      <Posts posts={posts} />
+      <Pagination nextUrl='/blog' nextText='More Posts' />
     </Container>
   )
 }
 
 export default Home
+
+export async function getStaticProps () {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
